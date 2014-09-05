b15f8548ba918b4841a50dfeb604542d1131e485
Lionel T <lio@iMac-di-Lionel.local>
2014-09-06 00:39:32 +0200

    script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function drag_start(event) {
     var style = window.getComputedStyle(event.target, null);
-    offset_data = [(parseInt(style.getPropertyValue("left"),10) - event.clientX),(parseInt(style.getPropertyValue("top"),10) - event.clientY)];
+    offset_data = [(parseInt(style.getPropertyValue("left"),10) - event.clientX),(parseInt(style.getPropertyValue("top"),10) - event.clientY), event.target.id];
     event.dataTransfer.setData("text/plain",offset_data);
 } 
 function drag_over(event) { 
@@ -21,22 +21,20 @@ function drag_over(event) {
     catch(e) {
         offset = offset_data.split(',');
     }
-    var dm = document.querySelectorAll('.dragme')[0];
-    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    var dm = document.getElementById(offset_data[2]);
+    dm.style.left = (event.clientX + parseInt(offset_data[0],10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset_data[1],10)) + 'px';
     event.preventDefault(); 
     return false; 
 } 
 function drop(event) { 
-  var coords = document.getElementById('coords');
-    var offset;
-    
-    var dm = document.querySelectorAll('.dragme')[0];
-    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+    var coords = document.getElementById('coords');
+    var dm = document.getElementById(offset_data[2]);
+    dm.style.left = (event.clientX + parseInt(offset_data[0],10)) + 'px';
+    dm.style.top = (event.clientY + parseInt(offset_data[1],10)) + 'px';
     coords.innerHTML = dm.style.left + ', ' + dm.style.top;
-    alert()
-    event.preventDefault();
+    // alert(offset_data[2])
+    event.preventDefault(); 
     
     return false;
 }  
